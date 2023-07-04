2e00eaa7f05c92195d7b631f5ef9ba89eda7efb0
unknown <jordipalacioseso@gmail.com>
2023-07-04 22:31:48 +0200

    fixContactUs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import './node_modules/eins-modal/dist/js/eins-modal.min';
-import './assets/js/jsDayNightMode.js';
-import './assets/js/AddRemoveClass.js';
-import './assets/js/jsHamburger.js';
 import './assets/css/index.css';
+import './node_modules/eins-modal/dist/js/eins-modal.min';
+// Los js se definen tal y como nos ayuda el autocompletado, van sin el .js
+import './assets/js/jsDayNightMode';
+import './assets/js/AddRemoveClass';
+import './assets/js/jsHamburger';
