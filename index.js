3329844e4b139fde3378b6ac5de22a11339897e0
unknown <jordipalacioseso@gmail.com>
2023-07-06 23:06:29 +0200

    decreaseScaleWhats_v3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './assets/css/index.css';
 import './node_modules/eins-modal/dist/js/eins-modal.min';
-import './assets/js/jsDayNightMode';
-// import './assets/js/AddRemoveClass.js';
-// import './assets/js/jsHamburger.js';
+import './assets/js/jsDayNightMode.js';
+import './assets/js/AddRemoveClass.js';
+import './assets/js/jsHamburger.js';
